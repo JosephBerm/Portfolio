af6cc7f0b0ce763fda7696d996a9eb618c880d09
Joseph Bermudez <josephbermudez13@gmail.com>
2023-01-22 22:45:32 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "../src/css/normalize.css";
+import "../src/css/App.css";
+
+function App() {
+	return (
+		<div className='App'>
+			<div>Hello</div>
+		</div>
+	);
+}
+
+export default App;

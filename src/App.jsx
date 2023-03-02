4923ad8f355d740a06cfc77edea96278a11c6acc
Joseph Bermudez <josephbermudez13@gmail.com>
2023-03-02 00:48:09 -0500

    Disappear navbar when scrolling
    When scroll down, hide, when up unhide. Must add animation and styling. to navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
 	return (
 		<div className='App'>
 			<Navbar />
+			<div className='large-box' />
 		</div>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../assets/Logo";
 
 function Navbar(props) {
@@ -20,30 +20,51 @@ function Navbar(props) {
 			location: "contact",
 		},
 	];
+
+	const [isVisible, setIsVisible] = useState(true);
+
+	useEffect(() => {
+		let prevScrollPos = window.pageYOffset;
+		const handleScroll = () => {
+			const currentScrollPos = window.pageYOffset;
+			if (prevScrollPos > currentScrollPos) {
+				setIsVisible(true);
+				//When setting is visible to true, check to see if the navbar should have a shadow
+				//a shadow is necessary when you're not at the very top of the page. When you're at the very top of the page, no shadow.
+				//height might not be the same. Ultimately, you might have to make the className dynamic as well.
+			} else {
+				setIsVisible(false);
+			}
+			prevScrollPos = currentScrollPos;
+		};
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
-		<header>
-			<nav>
-				<div className='logo'>
-					<a href='/'>
-						<Logo />
+		<nav className={isVisible ? "navbar" : "navbar hidden"}>
+			<div className='logo'>
+				<a href='/'>
+					<Logo />
+				</a>
+			</div>
+			<div className='nav_StyledLinks'>
+				<ol>
+					{routes.map((route, index) => (
+						<li key={index} style={{ "--index": index + 1 }}>
+							<a href={`/#${route.location}`}>{route.name}</a>
+						</li>
+					))}
+				</ol>
+				<div className='button-container' style={{ "--index": routes.length }}>
+					<a className='button' target='_blank' href='/resume.pdf'>
+						Resume
 					</a>
 				</div>
-				<div className='nav_StyledLinks'>
-					<ol>
-						{routes.map((route, index) => (
-							<li key={index} style={{ "--index": index + 1 }}>
-								<a href={`/#${route.location}`}>{route.name}</a>
-							</li>
-						))}
-					</ol>
-					<div className='button-container' style={{ "--index": routes.length }}>
-						<a className='button' target='_blank' href='/resume.pdf'>
-							Resume
-						</a>
-					</div>
-				</div>
-			</nav>
-		</header>
+			</div>
+		</nav>
 	);
 }
 export default Navbar;

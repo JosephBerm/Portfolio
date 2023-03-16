e6ce01bbb12790a22384ad47259eae7fd025b05a
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2023-03-16 08:55:38 -0400

    section-header styled
    Each header has a number in front of it and a line if there's space after it. Still unfinished.

--- src/App.jsx
+++ src/App.jsx
@@ -38,20 +38,31 @@ function App() {
 			{appRef && <Navbar scrolledRef={appRef} />}
 			<div id='content'>
 				<section className='hero_section'>
-					<div className='large-box'>Hello World</div>
+					<div className='intro'>
+						<h1>Hi, my name is</h1>
+						<h2>Joseph Bermudez.</h2>
+						<h3>I build things for the web</h3>
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi alias,
+							qui eius neque obcaecati exercitationem officiis, adipisci omnis ullam
+							libero maxime sed quidem blanditiis, deleniti doloremque eveniet. Nemo
+							doloribus ex sapiente. Libero eos eius quisquam. Veritatis iste error,
+							amet nam magnam vero tenetur repellat at, quam alias dolor iusto.
+						</p>
+						<button>Contact Me</button>
+					</div>
 				</section>
 				<section ref={aboutRef} id='about' className='about_section'>
-					<div className='large-box'>About</div>
-					<p>This is just a test!!</p>
+					<h2 className='section-header'>About Me</h2>
 				</section>
 				<section ref={jobsRef} id='jobs' className='jobs_section'>
-					<div className='large-box'>Jobs</div>
+					<h2 className='section-header'>Where I've Worked</h2>
 				</section>
 				<section ref={projectsRef} id='projects' className='projects_section'>
-					<div className='large-box'>Projects</div>
+					<h2 className='section-header'>Some Things I've Built</h2>
 				</section>
 				<section ref={contactRef} id='contact' className='contact_section'>
-					<div className='large-box'>Contact</div>
+					<h2 className='section-header'>Contact Me</h2>
 				</section>
 			</div>
 		</div>

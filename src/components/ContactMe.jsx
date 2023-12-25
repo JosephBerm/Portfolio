dc1d1345d9c4d45ec64b28b5b8dd3af720b828d9
Joseph Bermudez <42876560+JosephBerm@users.noreply.github.com>
2023-12-24 19:01:30 -0500

    Adding Subject Line to the email

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,29 +1,26 @@
-import React, { useContext } from "react";
-import PortfolioContext from "./../context/portfolioContext";
+import React, { useContext } from "react"
+import PortfolioContext from "./../context/portfolioContext"
 
 function ContactMe({ contactRef }) {
-	const { userProfile } = useContext(PortfolioContext);
+	const { userProfile } = useContext(PortfolioContext)
 	return (
-		<section
-			ref={contactRef}
-			id='contact'
-			className='section contact_section isNumbered'>
+		<section ref={contactRef} id='contact' className='section contact_section isNumbered'>
 			<h2 className='section-header lineless'>What's Next?</h2>
 			<h2 className='title'>Get In Touch</h2>
 			<p>
-				I'm always on the lookout for new ways to innovate. If you'd like to explore
-				an idea together or just want to say hi, feel free to shoot me an email. I'll
-				do my best to respond promptly and look forward to hearing from you!
+				I'm always on the lookout for new ways to innovate. If you'd like to explore an idea together or just want to
+				say hi, feel free to shoot me an email. I'll do my best to respond promptly and look forward to hearing from
+				you!
 			</p>
 			<a
 				className='button email-link'
-				href={`mailTo:${userProfile.email}`}
+				href={`mailto:${userProfile.email}?subject=Inquiry%20from%20Your%20Portfolio%20Website`}
 				rel='noopener noreferrer'
 				target='_blank'>
 				Say Hello
 			</a>
 		</section>
-	);
+	)
 }
 
-export default ContactMe;
+export default ContactMe
